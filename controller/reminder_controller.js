e966beb30ed96d47dc35784fabb2879cc2217312
enders-o <ender_sotelo@outlook.com>
2023-03-29 13:18:54 -0700

    authentication debugging

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -2,7 +2,10 @@ let database = require("../database");
 
 let remindersController = {
   list: (req, res) => {
-    res.render("reminder/index", { reminders: database.cindy.reminders });
+    user = database.find((user) => {
+      return user.id == 1
+    })
+    res.render("reminder/index", { reminders: user.reminders });
   },
 
   new: (req, res) => {
@@ -11,37 +14,37 @@ let remindersController = {
 
   listOne: (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = database.cindy.reminders.find(function (reminder) {
+    let searchResult = user.reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     });
     if (searchResult != undefined) {
       res.render("reminder/single-reminder", { reminderItem: searchResult });
     } else {
-      res.render("reminder/index", { reminders: database.cindy.reminders });
+      res.render("reminder/index", { reminders: user.reminders });
     }
   },
 
   create: (req, res) => {
     let reminder = {
-      id: database.cindy.reminders.length + 1,
+      id: user.reminders.length + 1,
       title: req.body.title,
       description: req.body.description,
       completed: false,
     };
-    database.cindy.reminders.push(reminder);
+    user.reminders.push(reminder);
     res.redirect("/reminders");
   },
 
   edit: (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = database.cindy.reminders.find((reminder) => {
+    let searchResult = user.reminders.find((reminder) => {
       return reminder.id == reminderToFind;
     });
     res.render("reminder/edit", { reminderItem: searchResult });
   },
 
   update: (req, res) => {
-    for (const reminder of database.cindy.reminders) {
+    for (const reminder of user.reminders) {
       if (reminder.id == req.params.id) {
         reminder.title = req.body.title
         reminder.description = req.body.description
@@ -56,15 +59,16 @@ let remindersController = {
   },
 
   delete: (req, res) => {
+    
     let reminderToFind = req.params.id;
 
-    let searchResult = database.cindy.reminders.find(function (reminder) {
+    let searchResult = user.reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     });
 
-    let reminderIndex = database.cindy.reminders.indexOf(searchResult)
+    let reminderIndex = user.reminders.indexOf(searchResult)
 
-    database.cindy.reminders.splice(reminderIndex, 1);
+    user.reminders.splice(reminderIndex, 1);
     res.redirect("/reminders");
   },
 };
--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const app = express();
 const path = require("path");
-const ejsLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const expressLayouts = require("express-ejs-layouts");
 const reminderController = require("./controller/reminder_controller");
-const authController = require("./controller/auth_controller");
+const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(express.urlencoded({ extended: false }));
-
-app.use(ejsLayouts);
-
 app.set("view engine", "ejs");
-
+app.use(expressLayouts);
 
 // Authentication
-const session = require("express-session");
-const { forwardAuthenticated, ensureAuthenticated } = require("./middleware/checkAuth");
+const authController = require("./controller/auth_controller");
+const { ensureAuthenticated, forwardAuthenticated } = require("./middleware/checkAuth");
+const passport = require("./middleware/passport");
+
 app.use(
   session({
     secret: "secret",
@@ -28,35 +26,51 @@ app.use(
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
-  );
-const passport = require("./middleware/passport")
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+app.use((req, res, next) => {
+  console.log(`User details are: `);
+  console.log(req.user);
+  
+  console.log("Entire session object:");
+  console.log(req.session);
+  
+  console.log(`Session details are: `);
+  console.log(req.session.passport);
+  next();
+});
+  
 // Routes start here
-app.get("/reminders",ensureAuthenticated, reminderController.list);
+
+app.get("/reminders", reminderController.list);
 
 app.get("/reminder/new", reminderController.new);
-
+  
 app.get("/reminder/:id", reminderController.listOne);
 
 app.get("/reminder/:id/edit", reminderController.edit);
 
 app.post("/reminder/", reminderController.create);
 
+// Implement this yourself
 app.post("/reminder/update/:id", reminderController.update);
 
+// Implement this yourself
 app.post("/reminder/delete/:id", reminderController.delete);
 
 // Fix this to work with passport! The registration does not need to work, you can use the fake database for this.
 app.get("/register", authController.register);
-app.get("/login",forwardAuthenticated, authController.login);
+app.get("/login", forwardAuthenticated, authController.login);
 app.post("/register", authController.registerSubmit);
 app.post("/login", authController.loginSubmit);
+
 
 app.listen(3001, function () {
   console.log(
     "Server running. Visit: localhost:3001/reminders in your browser 🚀"
-  );
-});
+    );
+  });
+  
