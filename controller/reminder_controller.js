0180287667d5011145445ee05f4ee6525e19f77d
Timur R <timurreziapov2@gmail.com>
2023-04-05 11:50:37 -0700

    Adds tags

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -28,6 +28,7 @@ let remindersController = {
       title: req.body.title,
       description: req.body.description,
       completed: false,
+      tags: req.body.tags,
     };
     req.user.reminders.push(reminder);
     res.redirect("/reminders");
@@ -46,6 +47,7 @@ let remindersController = {
       if (reminder.id == req.params.id) {
         reminder.title = req.body.title
         reminder.description = req.body.description
+        reminder.tags = req.body.tags
         if (req.body.completed == "true") {
           reminder.completed = true
         } else {
