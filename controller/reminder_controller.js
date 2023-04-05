4ce8373c7b55e667e5a23b4ad205c616e60f337c
Timur R <timurreziapov2@gmail.com>
2023-04-05 11:02:35 -0700

    testing

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -2,10 +2,7 @@ let database = require("../database");
 
 let remindersController = {
   list: (req, res) => {
-    user = database.find((user) => {
-      return user.id == 1
-    })
-    res.render("reminder/index", { reminders: user.reminders });
+    res.render("reminder/index", { reminders: req.user.reminders });
   },
 
   new: (req, res) => {
@@ -74,3 +71,5 @@ let remindersController = {
 };
 
 module.exports = remindersController;
+
+
